bb2193508095f9e3dd247c7a59f4518ce292577d
trinh5 <trinh.trinh@kms-solutions.asia>
2022-11-08 10:38:25 +0700

    add coe

---
+++ master/Object Repository/Page_AppointmentConfirmation/lbl_Comment.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lbl_Comment</name>
+   <tag></tag>
+   <elementGuidId>b70ba152-d5df-4e11-9c8a-4730577f03f8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'comment']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>comment</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ master/README.md
@@ -0,0 +1,3 @@
+# Visual Testing sample
+
+For tutorial, visit https://forum.katalon.com/t/update-with-katalon-studio-7-7-early-release-of-katalon-testops-visual-testing-image-comparison/45557.
